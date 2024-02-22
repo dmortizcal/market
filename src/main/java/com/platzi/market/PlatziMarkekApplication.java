03e765688d0e2f7e2424c75cdb7720bca5071694
dmortizcal <dmortizcal@gmail.com>
2024-02-22 17:40:56 -0500

    Init commit

---
+++ src/main/java/com/platzi/market/PlatziMarkekApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarkekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarkekApplication.class, args);
+	}
+
+}
